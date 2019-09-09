c106596e23d2b17a4d33dadad879ba44a8b65cd3
Ellana Lawrence <elawrence.blain@gmail.com>
2019-09-09 02:01:19 -0400

    2.16 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
+    var index = -1
+    var imageIndex = -1
+    let numberOfImages = 10
     //Code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +46,23 @@ class ViewController: UIViewController {
       
         index = newIndex
         messageLabel.text = messages[index]
+        
+        repeat{
+            newIndex = Int.random(in: 0..<numberOfImages)
+        } while imageIndex == newIndex
+        
+        imageIndex = newIndex
+        awesomeImageView.image = UIImage(named: "image\(imageIndex)")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
